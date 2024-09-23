0b00709d419ee2e3981aece3a94771ed3c9ddb03
lubos <LubosStrunc@email.cz>
2024-09-23 13:01:34 +0200

    Změna v navbaru a wiews

--- viewer/views.py
+++ viewer/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import ListView
 
-from viewer.models import CarMat
+from viewer.models import CarMat, Brand
 
 
 # Create your views here.
@@ -10,7 +10,19 @@ from viewer.models import CarMat
 
 def home(request):
     return render(request, "home.html")
-    # return HttpResponse('<ul>'
-    #                     '<li>Autokoberce</li>'
-    #                     '</ul>'
-    #                     )
+
+
+class CarMatsListView(ListView):
+    tamplate_name = "carmats.html"
+    model = CarMat
+    context_object_name = 'carmats'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        brands = Brand.objacts.all
+        context = ['carmats']
+        context = ['accessories']
+
+
+def carmat(request, pk):
+    pass
